87989f147f6c733b082136d8c8ecc61b5b29ce43
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-13 01:50:58 -0800

    Arrow Functions

--- ES6/es6.js
+++ ES6/es6.js
@@ -61,3 +61,74 @@ function doSmth(){
   let AGES;
   doSomth();
   console.log(AGES);
+
+  //ARROW FUNCTIONS (FAT)
+
+function fn(){
+    console.log('Hello \n World');
+  }
+  fn();
+  
+  // in arrow wAY
+  
+  var func = () => {
+  console.log('Hello \n Goddey')
+  }
+  func();
+  
+  // Alternative 1
+  
+  var funct = () => console.log('Hello \n World!');
+  funct();
+  
+  // Alternative 2: if one line of code
+  
+  var functn = () => 'Hello \n Prez JJ'; console.log(functn());
+  
+  //Example
+  
+  var example = () => {
+     let a = 2;
+     let b = 3;
+     return a + b;
+  };
+  
+  console.log(example());
+  
+  // Alternative Method 1;
+  
+  var exampleAlt1 = (a, b) => {
+    return a + b;
+  };
+  
+  //console.dir(exampleAlt1(4,5));
+  console.log(exampleAlt1(4,5));
+  
+  
+  // Alternative Method 2;
+  
+  var exampleAlt2 = (a, b) => a + b;
+  
+  //console.dir(exampleAlt1(4,5));
+  console.log(exampleAlt2(4,6));
+  
+  
+  // Alternative Method 3;
+  
+  var exampleAlt3 = (a, b) => a + b;
+  
+  //console.dir(exampleAlt1(4,5));
+  console.log(exampleAlt3(4,10));
+  
+  
+  // Alternative Method 4;
+  
+  //var exampleAlt4 = (a) => a + 7;
+  
+  var exampleAlt4 = a => a + 7; // Alt
+  console.log(exampleAlt4(4));
+  
+  
+  // Using arrow as call back
+  
+  setTimeout(() => console.log('Hello World!!!'), 1000);
