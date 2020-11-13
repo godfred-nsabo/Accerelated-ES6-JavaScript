061619b8fe348a209c8051553f7a7a1497fa1acd
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-13 01:13:01 -0800

    Constants with Const

--- ES6/es6.js
+++ ES6/es6.js
@@ -14,3 +14,21 @@ console.log(age);
 //console.log(name);
 
 console.log(age);
+
+
+//Constants with 'Const' 
+
+//const Age = 27;
+//Age  = 29
+
+const AGES = [27,29,31];
+console.log(AGES);
+AGES.push(25);
+console.log(AGES);
+
+const OBJ = {
+age: 27
+};
+console.log(OBJ);
+OBJ.age = 30;
+console.log(OBJ);
