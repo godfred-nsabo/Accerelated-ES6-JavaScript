e42d519a49faf5a9365e65914289cdebbb15fd23
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-13 00:40:18 -0800

    ES6 syntax

---
+++ ES6/es6.js
@@ -0,0 +1,16 @@
+//Let & BLock Scope
+//console.log('something');
+
+//var name = "Goddey";
+//let name = "Goddey";
+//console.log(name);
+
+let age = 39;
+if (true) {
+  let name = "Goddey";
+  let age = 83;
+console.log(age);
+}
+//console.log(name);
+
+console.log(age);
